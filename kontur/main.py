7bfa2d2941b37d8b850125a26f96c08fe4158256
mustafa kundakçı <kundakcici@gmail.com>
2022-04-24 20:21:55 +0300

    ekleme

---
+++ kontur/main.py
@@ -0,0 +1,16 @@
+import cv2
+from cv2 import COLOR_BGR2GRAY
+
+img = cv2.imread("contour1.png")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+contours,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(img,contours,-1,(0,0,255),1)
+
+cv2.imshow('counter',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
